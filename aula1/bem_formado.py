9dbd5c62fc54357b20742e22da1b94a2f74d62d2
Lucas de Oliveira <lucassdeoliveira@outlook.com.br>
2025-01-22 18:29:37 -0300

    aula 1: questão de formacao correta

--- aula1/bem_formado.py
+++ aula1/bem_formado.py
@@ -1,2 +1,27 @@
-def verificar():
+from typing import List
+
+sequencia = List[str]
+
+def bem_formado(vetor: sequencia) -> bool:
     
+    pares = {'(' : ')', '[' : ']', '{' : '}'}
+    pilha = ""
+
+    for caracter in vetor:
+       
+        if caracter in "([{":
+            pilha += caracter
+
+        elif caracter in ")]}":
+            if not pilha:
+                return False
+            if pares[pilha[-1]] == caracter:
+                pilha = pilha[:-1]
+            else:
+                return False
+    
+    return True if not pilha else False
+                
+
+print(bem_formado(['{', '(', ')', '}']))
+print(bem_formado(['{', '(', '(', '{', '}', ')', ')', '}']))
